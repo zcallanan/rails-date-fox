b50228e9ea35b5c030ecf6f155f6ffab429b4941
Zach Callanan <caveofmonsters@gmail.com>
2020-05-21 08:22:57 +0200

    controller changes

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -3,9 +3,49 @@ class ExperiencesController < ApplicationController
 
   def index
     @experiences = Experience.all
-    @search = Search.new
-    @item_kinds = ItemKind.all
+    @items = Item.all
 
+    @price_range = params[:price_range]
+    @starts_at = params[:starts_at]
+    @ends_at = params[:ends_at]
+    @activity_array = params[:activity_ids]
+
+    # sum = 0
+    # @activity_array.each do |activity|
+    #   sum += activity.duration
+    # end
+    # average_duration = sum / @activity_kinds.size
+
+
+    # @items = Item.where("price_range = ?", "#{@price_range.size}").order(rating: :desc)
+    # model would be...
+    # scope :search_by_price_range, lambda { |price|
+    #   where("price_range = ?", "#{price}")
+    # }
+    # controller/service
+    # @items = @items.search_by_price_range(@price_range.size)
+
+
+
+
+    # what activities you have selected 1 activity = 1 item per experience
+    # Calculate duration from search starts and ends datetime
+    # With total duration, compare against duration of experience + travel time default to generate number of items
+    # filter by price first, then duration, then rating
+    ## top average duration
+    ## if it exceeds desired duration, remove 1 or go with lowest duration items
+    # @message activity you are looking for exceeds your duration
+    # select items with the best rating
+    # add items to an array
+    # If user wants a different item
+    # @item = Item.find(params[:id])
+
+    # form is sending params
+    # filter items by price_range params
+    # sort items by rating params
+    # group items by activity type
+    # randomly select one item per activity type and store in one of three arrays
+    ## what activities selected, for each activity get top ranked items and store them in arrays
   end
 
   def show; end
