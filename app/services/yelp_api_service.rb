73f9039b403ebaa40ca85240a44347576d737235
Andy B <andybarnov@gmail.com>
2020-05-21 16:40:10 +0200

    API Wrapper and seeds

---
+++ app/services/yelp_api_service.rb
@@ -0,0 +1,64 @@
+require "open-uri"
+
+class YelpApiService
+  API_KEY = ENV.fetch("YELP_API")
+  BASE_URI = "https://api.yelp.com/v3/businesses/"
+
+  def initialize(attrs = {})
+    @location = attrs[:location]
+    @radius = attrs[:radius]
+    @category = attrs[:category]
+    @items = []
+  end
+
+  def call
+    url = BASE_URI + "search?location=#{@location}&radius=#{@radius}&categories=#{@category}"
+    serialized_data = URI.open(url, "Authorization" => "Bearer #{API_KEY}").read
+    data = JSON.parse(serialized_data)
+    data = data["businesses"]
+    data.each do |row|
+      location = row["location"]
+      address_string = "#{location["address1"]}, #{location["city"]} #{location["zip_code"]}"
+      price = row["price"].nil? ? 1 : row["price"].size
+      item = Item.find_by(name: row["name"])
+
+      # Only make new request if the same item is not already in a DB
+      if item.nil?
+        # A member call will only be made if item does not already exist
+        item = Item.create!(
+          name: row["name"],
+          description: "",
+          address: address_string,
+          rating: row["rating"],
+          price_range: price,
+          review_count: row["review_count"]
+        )
+
+        # Make a member request for item
+        url = BASE_URI + row["id"]
+        ser_data = URI.open(url, "Authorization" => "Bearer #{API_KEY}").read
+        item_data = JSON.parse(ser_data)
+
+        # Create item photos
+        item.photos.create!(item_data["photos"].map { |url| {url: url} })
+
+        # Create opening hours
+        if item_data.key?("hours")
+          item_data["hours"][0]["open"].each do |h|
+            item.operating_hours << OperatingHour.new(
+              open_time: h["start"].sub(/^(\d{1,2})(\d{2})$/, '\1:\2').to_time,
+              close_time: h["end"].sub(/^(\d{1,2})(\d{2})$/, '\1:\2').to_time,
+              day: h["day"]
+            )
+          end
+        end
+      end
+
+      @items << item
+      return @items
+
+    rescue OpenURI::HTTPError
+      puts "Request failed, carry on"
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 # starts_at & ends_at are datetime type
 
-require 'json'
-require 'open-uri'
-require 'csv'
-API_KEY = ENV.fetch('YELP_API')
+require "csv"
 
-# Activity.destroy_all
-# Item.destroy_all
-# OperatingHour.destroy_all
-# ItemOperatingHour.destroy_all
+ItemOperatingHour.destroy_all
+OperatingHour.destroy_all
+Photo.destroy_all
+Item.destroy_all
+Activity.destroy_all
+Experience.destroy_all
+
 n = 0
 3.times do
   n += 1
@@ -27,113 +19,39 @@ n = 0
 end
 
 activities = [
-  ['Dinner & Lunch', 120],
-  ['Bar', 90],
-  ['Club & Dance', 90],
-  ['Breakfast', 75],
-  ['Events, Shows & Movies', 150],
-  ['Theatres & Operas', 180],
-  ['Concerts & Festivals', 180],
-  ['Museums & Sites', 120],
-  ['Indoor Activity', 90],
-  ['Outdoor Activity', 180]
+  ["Dinner & Lunch", 120],
+  ["Bar", 90],
+  ["Club & Dance", 90],
+  ["Breakfast", 75],
+  ["Events, Shows & Movies", 150],
+  ["Theatres & Operas", 180],
+  ["Concerts & Festivals", 180],
+  ["Museums & Sites", 120],
+  ["Indoor Activity", 90],
+  ["Outdoor Activity", 180]
 ]
 
 # SEED ACTIVITIES
 activities.each do |value|
-  activity = Activity.new(
+  Activity.create!(
     name: value[0],
     duration: value[1]
   )
-  # SAVE
-  activity.save
 end
 
-def item_operating_hours(item, operating_hours)
-  # SEED JOIN TABLE
-      item_operating_hours = ItemOperatingHour.new
+csv_read_options = {col_sep: ",", quote_char: '"', headers: :first_row}
 
-      item_operating_hours.item = item
-      item_operating_hours.operating_hour = operating_hours
-
-      # SAVE
-      item_operating_hours.save
-end
-
-def operating_hours(array, item)
-  array.each do |hour|
-    operating_hours = OperatingHour.new(
-      day: hour['day'],
-      open_time: hour['start'],
-      close_time: hour['end']
-    )
-    # SAVE
-    operating_hours.save
-    item_operating_hours(item, operating_hours)
-  end
-end
-
-csv_read_options = { col_sep: ',', quote_char: '"', headers: :first_row }
-
-CSV.foreach('categories.csv', csv_read_options) do |csv_row|
+CSV.foreach("categories.csv", csv_read_options) do |csv_row|
   puts csv_row[0]
-  url = "https://api.yelp.com/v3/businesses/search?location=Munich&radius=10000&categories=#{csv_row[0]}"
-  # url = 'https://api.yelp.com/v3/businesses/search?location=Munich&radius=10000&categories=restaurants'
-  serialized_data = open(url, 'Authorization' => "Bearer #{API_KEY}").read
-  data = JSON.parse(serialized_data)
-  data['businesses'].each do |row|
-    puts row['id']
-    location = row['location']
-    address_two = " #{location['address2']}" unless location['address2'].nil?
-    address_three = " #{location['address3']}" unless location['address3'].nil?
-    address_string = "#{location['address1']}#{address_two}#{address_three}, #{location['city']} #{location['zip_code']}"
-    row['price'].nil? ? price = 1 : price = row['price'].size
-    item = Item.new(
-      name: row['name'],
-      description: 'A cool place to eat',
-      address: address_string,
-      availability: true,
-      rating: row['rating'],
-      price_range: price,
-      review_count: row['review_count']
-    )
-    # act = Activity.all
-    # item.activity = act[0]
-    activities = Activity.all
-    activities.each do |activity|
-      item.activity = activity if activity.name == csv_row[1]
-    end
+  items = YelpApiService.new(
+    location: "Munich",
+    radius: 10_000,
+    category: csv_row[0]
+  ).call
 
-    # SAVE
-    item.save
-    puts 'saved'
-
-    url = "https://api.yelp.com/v3/businesses/#{row['id']}"
-    # url = "https://api.yelp.com/v3/businesses/HI7M_qC-q2P7U8a7kIfW6g"
-
-    ser_data = open(url, 'Authorization' => "Bearer #{API_KEY}").read
-    item_data = JSON.parse(ser_data)
-
-    # SEED OPERATING HOURS
-    if item_data.key?('hours')
-      hours = item_data['hours'][0]['open']
-      operating_hours(hours, item)
-    else
-      operating_hours([{ 'start' => '1200', 'end' => '1200', 'day' => 0 }], item)
-    end
-
-    images = item_data['photos']
-    images.each_with_index do |photo_url, ind|
-      # SEED PHOTOS
-      puts photo_url
-      file = URI.open(photo_url)
-      item.photos.attach(
-        io: file,
-        filename: "#{row['name']}_#{ind}",
-        content_type: 'image/jpg'
-      )
-    end
+  items.each do |item|
+    item.update(activity: Activity.find_by(name: csv_row[1]))
   end
 end
 
-puts 'Finished!'
+puts "Finished!"
