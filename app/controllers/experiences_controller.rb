68c76a29e5e8958f997cfd82aa7d32f199d16789
Zach Callanan <caveofmonsters@gmail.com>
2020-05-21 08:24:45 +0200

    missing instance variables

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -4,6 +4,8 @@ class ExperiencesController < ApplicationController
   def index
     @experiences = Experience.all
     @items = Item.all
+    @search = Search.new
+    @item_kinds = ItemKind.all
 
     @price_range = params[:price_range]
     @starts_at = params[:starts_at]
