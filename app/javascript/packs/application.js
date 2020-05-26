b3e55a9a0b1e019dfe0e001952d2f7f8ed38c078
Sven Haider <sven.haider@web.de>
2020-05-26 15:31:32 +0200

    work on form styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   select_city();
+  select_price();
   choice();
   progress();
 });
@@ -26,5 +27,5 @@ document.addEventListener('turbolinks:load', () => {
 
 import 'plugins/flatpickr'
 import "controllers"
-import { choice, select_city } from '../components/checkboxes'
+import { choice, select_city, select_price } from '../components/checkboxes'
 import { progress } from '../components/progress_bar'
