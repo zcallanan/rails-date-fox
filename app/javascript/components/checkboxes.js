66755f9b5f6a77541ab30eb0ddfd3df8325b8813
Sven Haider <sven.haider@web.de>
2020-05-26 22:07:31 +0200

    quick-fix for selecting cities in form

--- app/javascript/components/checkboxes.js
+++ app/javascript/components/checkboxes.js
@@ -7,48 +7,37 @@ const choice = () => {
       event.currentTarget.classList.toggle("active");
     });
   });
-
 };
-
 
 // Adding a class to an element and removing it from the previous one
 const select_city = () => {
   const cities = document.querySelectorAll('.city');
 
-    cities.forEach((city) => {
-      
-      city.addEventListener('click', (event) => {
-        cities.forEach((city) => {
-          city.classList.remove("activated")
-        });
-        // if (event.currentTarget.classList.contains('active')) {
-        //   event.currentTarget.classList.remove("active")
-        // };
-        event.currentTarget.classList.add("activated"); 
-      });
-
-    });
+  cities.forEach((city) => {
     
+    city.addEventListener('click', (event) => {
+      cities.forEach((city) => {
+        city.classList.remove("activated")
+      });
+      event.currentTarget.classList.add("activated"); 
+    });
+  });
 };
 
 // Select price range
-
 const select_price = () => {
   const price_range = document.querySelectorAll('.price-choice');
 
-    price_range.forEach((price) => {
-      
-      price.addEventListener('click', (event) => {
-        price_range.forEach((price) => {
-          price.classList.remove("active")
-        });
-        event.currentTarget.classList.add("active"); 
+  price_range.forEach((price) => {
+
+    price.addEventListener('click', (event) => {
+      price_range.forEach((price) => {
+        price.classList.remove("active")
       });
-
+      event.currentTarget.classList.add("active"); 
     });
-    
+  });
 };
-
 
 export { choice }
 export { select_city }
