f8d6e6ee7a262b21539f77e4d6b053710eeb3403
Zach Callanan <caveofmonsters@gmail.com>
2020-05-27 12:04:53 +0200

    configured bookings controller

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -3,4 +3,6 @@ class Experience < ApplicationRecord
   has_many :items, through: :item_experiences, dependent: :destroy
   has_many :search_experiences
   has_many :searches, through: :search_experiences
+  has_many :bookings
+  has_many :users, through: :bookings
 end
