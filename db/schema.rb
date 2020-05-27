5c1aa1e75f39c7a5587f18b2a7996b3f3d36e49d
Marc Lueers <marc.andre.lueers@gmail.com>
2020-05-27 18:45:47 +0200

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_075631) do
+ActiveRecord::Schema.define(version: 2020_05_26_175109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -171,7 +171,6 @@ ActiveRecord::Schema.define(version: 2020_05_27_075631) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "city"
-    t.integer "experience_focus"
     t.index ["user_id"], name: "index_searches_on_user_id"
   end
 
